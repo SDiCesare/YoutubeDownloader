53e48e87858b35f830ab618e5704d9119de5e57c
SDiCesare <SDiCesare@users.noreply.github.com>
2021-10-09 11:50:40 +0200

    Main
    Main structure

---
+++ src/com/ike/youtubedownloader/Main.java
@@ -0,0 +1,13 @@
+package com.ike.youtubedownloader;
+
+/**
+ * @author Ike
+ * @version 1.0A
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
